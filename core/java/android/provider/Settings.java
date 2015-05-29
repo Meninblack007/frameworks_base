02887beda38f4b48d3eae7da8656702ca15fde1c
existancevip <existancevip@gmail.com>
2015-05-28 21:03:47 -0400

    Revert "Force Expanded Notifications (1/2)"
    This reverts commit caef786327a1bea99f3e38988d21251249906b8e.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3797,12 +3797,6 @@ public final class Settings {
         public static final String ANIMATION_CONTROLS_REVERSE_EXIT = "animation_controls_reverse_exit";
 
         /**
-         * Force expanded notifications on all apps that support it.
-         * @hide
-         */
-        public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
